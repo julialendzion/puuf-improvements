fe7723d72d625942d2e613e5bf0e144078628854
Julia Lendzion <juli47g3@stud.kea.dk>
2022-06-12 15:26:33 +0200

    fixed on click dropdown

--- script/main.js
+++ script/main.js
@@ -48,19 +48,3 @@ const handleScrollAnimation = () => {
 window.addEventListener("scroll", () => {
   handleScrollAnimation();
 });
-
-//dropdown
-let acc = document.getElementsByClassName("type");
-let i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    let panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
